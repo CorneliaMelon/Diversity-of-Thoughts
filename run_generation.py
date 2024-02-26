6f09dba0bce1e6950c47a340dcede298058e143c
Cornelia Weinzierl <cornelia@meloncommunity.com>
2024-02-26 17:22:22 +0100

    Generated dataset - see output csv

--- run_generation.py
+++ run_generation.py
@@ -1,13 +1,20 @@
 import data_generation
+import openai
 
 
-QUERY = "" 
 
-ROLES = ["Assistant", "Assistant", "Assistant", "Assistant"]
+
+openai.api_key = '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'
+
+output_file_path = 'output.csv'
+
+QUERY = "A standard six-sided fair die is rolled four times. The probability that the product of all four numbers rolled is a perfect square is $\\tfrac{m}{n}$, where $m$ and $n$ are relatively prime positive integers. Find $m+n$.\n" 
+
+ROLES = ["Mathematician", "Economist", "Programmer", "Historian"]
 #need function that gives us the role description based off of the key 
 
 def main():
-    construct_training_data(QUERY, ROLES)
+    data_generation.construct_training_data(QUERY, ROLES, data_generation.ROLE_MAP, openai, output_file_path)
     
 
 
