cc44c9bbd5fda2801261f39648b8e95f0b33fcd4
Cornelia Weinzierl <cornelia@meloncommunity.com>
2024-03-03 10:53:16 +0100

    280 dataset generated in output.csv

--- run_generation.py
+++ run_generation.py
@@ -8,7 +8,7 @@ openai.api_key = ''
 
 output_file_path = 'output.csv'
 
-QUERY = "A standard six-sided fair die is rolled four times. The probability that the product of all four numbers rolled is a perfect square is $\\tfrac{m}{n}$, where $m$ and $n$ are relatively prime positive integers. Find $m+n$.\n" 
+QUERY = "Solve\n\\[\\arcsin x + \\arcsin 2x = \\frac{\\pi}{3}.\\]" 
 
 ROLES = ["Mathematician", "Economist", "Programmer", "Historian"]
 #need function that gives us the role description based off of the key 
