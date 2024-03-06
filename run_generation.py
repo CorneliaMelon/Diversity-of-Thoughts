1035292e2f49419a0f78f6c050a5f73aab7371ba
Cornelia Weinzierl <cornelia@meloncommunity.com>
2024-03-07 00:39:39 +0100

    Generated Output 500-4-roles and Output-1000-4-roles Dataset

--- run_generation.py
+++ run_generation.py
@@ -9,12 +9,12 @@ import csv
 
 openai.api_key = ''
 
-output_file_path = 'output-new.csv'
+output_file_path = 'output-500-4-roles.csv'
 
 output_problem_path = 'problems.csv'
 
 
-
+#batch_size = 10
 batch_size = 8
 
 #QUERY = "A standard six-sided fair die is rolled four times. The probability that the product of all four numbers rolled is a perfect square is $\\tfrac{m}{n}$, where $m$ and $n$ are relatively prime positive integers. Find $m+n$.\n" 
@@ -24,7 +24,7 @@ batch_size = 8
 #directory = '/Users/corneliaweinzierl/Downloads/MATH/train/number_theory'
 #directory = '/Users/corneliaweinzierl/Downloads/MATH/train/prealgebra'
 #directory = '/Users/corneliaweinzierl/Downloads/MATH/train/precalculus'
-directory = '/Users/corneliaweinzierl/Downloads/MATH/train/intermediate_algebra'
+#directory = '/Users/corneliaweinzierl/Downloads/MATH/train/intermediate_algebra'
 
 ROLES = ["Mathematician", "Programmer", "Economist", "Lawyer"]
 
@@ -74,9 +74,9 @@ def append_csv_files(source_csv_1, source_csv_2, destination_csv):
             for row in csv_reader2:
                 csv_writer.writerow(row)
 
-source_csv_1 = 'output.csv'
-source_csv_2 = 'output-new.csv'
-destination_csv = 'output-504.csv'
+source_csv_1 = 'output-500-4-roles.csv'
+source_csv_2 = 'output-504.csv'
+destination_csv = 'output-1000-4-roles.csv'
 
 def main():
     #process_random_files(directory = directory, batch_size = batch_size, output_problem_path = output_problem_path)
